569cfb8052bb8f8f6c6aecf61fd21d26e17678b3
EC2 Default User <andreinew@mail.ru>
2019-10-03 13:35:08 +0000

    Setup SQLite from expo-eqlite package and added basic db initialization logic

--- App.js
+++ App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import ReduxThunk from 'redux-thunk';
-import placesReducer from './src/store/places-reducer'
+import placesReducer from './src/store/places-reducer';
+import { init } from '/src/helpers/db';
 
+
+
+init().then(() => {
+  console.log('Initialized database');
+}).catch(err => {
+  console.log('Initialalizing db failed');
+  console.log(err);
+});
 
 import PlacesNavigator from './src/navigation/PlacesNavigator';
 
