d0a434f19176f339e38fafef3ccb2ba79f152b7f
EC2 Default User <andreinew@mail.ru>
2019-10-04 13:25:21 +0000

    Added map marker logic

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const MapScreen = props => {
+  const [selectedLocation, setSelectedLocation] = useState();
   const mapRegion = {
-    latitude: 37.78,
-    longitude: 122.43,
+    latitude: 56.12,
+    longitude: 37.31,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.9421
   }
 
+  const selectLocationHandler = event => {
+
+    setSelectedLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude
+    })
+  }
+
+  let markerCoordinates;
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng
+    }
+  }
+
   return (
-    <MapView style={styles.map} region={mapRegion}/>
+    <MapView style={styles.map} region={mapRegion} onPress={selectLocationHandler}>
+      { markerCoordinates && <Marker title='Picked Location' coordinate={markerCoordinates}></Marker>}
+    </MapView>
   );
 };
 const styles = StyleSheet.create({
