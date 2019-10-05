22bd869bf80313634c9f3bf78925173f7f61dde2
AndreyKonovalenko <andreinew@mail.ru>
2019-10-05 19:53:02 +0300

    Added addres storing with Reverse Geocoding api logic

--- src/store/places-reducer.js
+++ src/store/places-reducer.js
@@ -11,17 +11,26 @@ export default (state = initialState, action) => {
       const newPlace = new Place(
         action.placeData.id.toString(),
         action.placeData.title,
-        action.placeData.image
+        action.placeData.image,
+        action.placeData.address,
+        action.placeData.coords.lat,
+        action.placeData.coords.lng
       );
       return {
         places: state.places.concat(newPlace)
       };
-
     case SET_PLACES:
       return {
         places: action.places.map(
           element =>
-            new Place(element.id.toString(), element.title, element.imageUri)
+            new Place(
+              element.id.toString(),
+              element.title,
+              element.imageUri,
+              element.address,
+              element.lat,
+              element.lng
+            )
         )
       };
     default:
